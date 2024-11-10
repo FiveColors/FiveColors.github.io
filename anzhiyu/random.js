10531dc2730cdb842d04017850fb110ff87fcb35
FiveColors <bingwenwu@163.com>
2024-11-10 20:05:34 +0800

    Site updated: 2024-11-10 20:05:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/10/hello-world/"];function toRandomPost(){
+var posts=["2024/11/10/hello-world/","2022/07/02/Python-os-listdir-方法/","2022/07/01/warnings —— 警告信息的控制/","2024/11/10/append（）函数的用法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
