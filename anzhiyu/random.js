0575c798be54cf4780d75791f16e1ea5f4eec15b
FiveColors <bingwenwu@163.com>
2024-11-12 19:45:34 +0800

    Site updated: 2024-11-12 19:45:32

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/11/10/hello-world/","2022/07/02/Python-os-listdir-方法/","2022/07/01/warnings —— 警告信息的控制/","2024/11/10/append（）函数的用法/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
